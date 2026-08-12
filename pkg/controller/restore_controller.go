eda80e9e6a685d32bfd68bd8789a3d8885a1ca68
Ayush <bhandariayush935@gmail.com>
2026-08-13 00:00:07 +0530

    Refactor: Remove legacy resticrepositories.velero.io hardcoding
    Signed-off-by: Ayush <bhandariayush935@gmail.com>

--- pkg/controller/restore_controller.go
+++ pkg/controller/restore_controller.go
@@ -79,12 +79,6 @@ var nonRestorableResources = []string{
 	// https://github.com/vmware-tanzu/velero/issues/622
 	"restores.velero.io",
 
-	// TODO: Remove this in v1.11 or v1.12
-	// Restic repositories are automatically managed by Velero and will be automatically
-	// created as needed if they don't exist.
-	// https://github.com/vmware-tanzu/velero/issues/1113
-	"resticrepositories.velero.io",
-
 	// CSINode delegates cluster node for CSI operation.
 	// VolumeAttachement records PV mounts to which node.
 	// https://github.com/vmware-tanzu/velero/issues/4823
